d5522783d8da8eb0a7eff75fa9217427f1c60dca
J-Gambler <antonovserg943@gmail.com>
2021-11-22 15:50:05 +0300

    style avatar

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,6 +1,44 @@
-const Avatar = () => {
+import { useRef, useEffect, useState } from 'react';
+import { Col, Row } from 'react-bootstrap';
+
+const Avatar = (props) => {
+
+    const imageRef = useRef(null);
+
+    const changeImage = () => {
+
+    }
+
+    const handleImage = () => {
+
+    }
+
     return (
-        <div></div>
+        <Row className="avatar">
+            <Col lg={5}>
+                <h1 className="title">{ props.title }</h1>
+                <p className="content">{ props.content }</p>
+            </Col>
+            <Col lg={3}>
+                <input id="input-image" accept="image/*" type="file" name="image" className="d-none" onChange={changeImage} />
+                <img ref={imageRef} src={require('../assets/img/avatar.png').default} onClick={handleImage}/>
+            </Col>
+            <Col lg={4}>
+                <p className="description">
+                    <span>submitted by:</span> <br />
+                    <span>submission date:</span> <br />
+                    <span>total projects submitted:</span> <br />
+                    <span>total projects submitted:</span> <br />
+                    <span>nodestones held:</span>
+                </p>
+            </Col>
+            {/* <h1>{ props.title }</h1>
+            <p>
+                <span>{ props.content }</span>
+                <input id="input-image" accept="image/*" type="file" name="image" className="d-none" onChange={changeImage} />
+                <img ref={imageRef} src={require('../assets/img/avatar.png').default} onClick={handleImage}/>
+            </p> */}
+        </Row>
     );
 }
 
