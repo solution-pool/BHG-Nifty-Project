97101e64339b91eb7e82e414d7735ad47d88ba4d
J-Gambler <antonovserg943@gmail.com>
2021-11-25 16:32:24 +0300

    get user info when start project

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ import Project from './pages/Project';
 import Proposal from './pages/Proposal';
 import Outside from './pages/Outside';
 import Web3 from 'web3';
+import { database } from './config/firebase';
 import { useState, useEffect } from 'react';
 
 function App() {
   
   const [walletAddress, setAddress] = useState('');
+  const [userInfo, setUserInfo] = useState({})
     
   useEffect( () => {
       walletConnect()
-  }, [walletAddress] )
+  }, [walletAddress, userInfo ? userInfo.username : userInfo] )
 
   const walletConnect = async () => {
       if(window.ethereum) {
@@ -22,21 +24,46 @@ function App() {
           await window.ethereum.enable()
           const account = await window.web3.eth.getAccounts()
           setAddress(account[0])
+          if(account[0]) {
+            getUserInfo(account[0])
+          }
       } else if(window.web3) {
           window.web3 = new Web3(window.web3.currentProvider) 
           const account = await window.web3.eth.getAccounts()
           setAddress(account[0])
+          if(account[0]) {
+            getUserInfo(account[0])
+          }
       } else {
           window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
       }
   }
+
+  const getUserInfo = (accountAddress) => {
+    const userRef = database.ref('member_profile')
+    userRef.get().then( (snapshot) => {
+      if(snapshot.exists) {
+        const newAry = snapshot.val()
+        if(newAry) {
+
+            for(let i in newAry) {
+                let data = newAry[i]
+                if(data.username == accountAddress) {
+                  setUserInfo(data)
+                  break
+                }
+            }
+        }
+    }
+    })
+  }
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home walletConnect={walletConnect} walletAddress={walletAddress} />} />
-        <Route path="project/:id/:t" element={<Project walletConnect={walletConnect} walletAddress={walletAddress} />} />
-        <Route path="proposal" element={<Proposal walletConnect={walletConnect} walletAddress={walletAddress} />} />
-        <Route path="outside" element={<Outside walletConnect={walletConnect} walletAddress={walletAddress} />} />      
+        <Route path="/" element={<Home walletConnect={walletConnect} walletAddress={walletAddress} userInfo={userInfo} />} />
+        <Route path="project/:id/:t" element={<Project walletConnect={walletConnect} walletAddress={walletAddress} userInfo={userInfo} />} />
+        <Route path="proposal" element={<Proposal walletConnect={walletConnect} walletAddress={walletAddress} userInfo={userInfo} />} />
+        <Route path="outside" element={<Outside walletConnect={walletConnect} walletAddress={walletAddress} userInfo={userInfo} />} />      
       </Routes>
     </BrowserRouter>
   );
