f50915fc3e4bb3632e6bab0faf8eb50b24d5402d
J-Gambler <antonovserg943@gmail.com>
2021-11-26 16:13:02 +0300

    set ui block in the outside project

--- src/pages/Outside.js
+++ src/pages/Outside.js
@@ -6,6 +6,9 @@ import { Link } from 'react-router-dom';
 import { database, storage } from '../config/firebase';
 import {NotificationContainer, NotificationManager} from 'react-notifications';
 import 'react-notifications/lib/notifications.css';
+import BlockUi from 'react-block-ui';
+import 'react-block-ui/style.css';
+import ReactHtmlParser from 'react-html-parser'
 
 const Outside = (props) => {
     const title     = "Outside Project Submission";
@@ -25,6 +28,8 @@ const Outside = (props) => {
     const [inputFile, setInputFile] = useState({});
     const [isLoading, setLoading] = useState(false);
     const [btnTitle, setBtnTitle] = useState('Choose File');
+    const [blocking, setBlock] = useState(false)
+    const [message, setMessage] = useState('Checking connnection...')
 
     const reset = () => {
         setProjectName('')
@@ -43,8 +48,21 @@ const Outside = (props) => {
     }
 
     useEffect( () => {
+
+        if(props.userLoad) {
+            if(props.userInfo.username) {
+                setBlock(false)
+                setMessage('Loading...')
+            } else {
+                setMessage(ReactHtmlParser("You are not registered as a Nifty member. Please sign up first. <a href='/'> Back </a>"))
+                setBlock(true)
+            }
+        } else {
+            setBlock(true)
+            setMessage('Checking connnection...')
+        }
         setInputFile(document.getElementById('input-file'))
-    } )
+    }, [props.userLoad] )
 
     const changeProjectName = (e) => {
         setProjectName(e.target.value)
@@ -175,92 +193,94 @@ const Outside = (props) => {
                 <NotificationContainer />
                 <Avatar title={title} content={content} userInfo={props.userInfo} />
                 <Row className="content">
-                    <Form onSubmit={handleSubmit} encType="multipart/form-data">
-                        <Row>
-                            <Col lg="4" md="6" sm="12" className="main-col">
-                                <Form.Group controlId="formProjectName">
-                                    <Form.Label>Project Name</Form.Label>
-                                    <Form.Control type="text" placeholder="Name of Project" value={projectName} onChange={changeProjectName} required />
-                                </Form.Group>
-                            </Col>
-                            <Col lg="4" md="6" sm="12" className="main-col">
-                                <Form.Group controlId="formSupply">
-                                    <Form.Label>Supply</Form.Label>
-                                    <Form.Control type="text" placeholder="How many?" value={supply} onChange={changeSupply} required />
-                                </Form.Group>
-                            </Col>
-                            <Col lg="4" md="6" sm="12" className="main-col">
-                                <Form.Group controlId="formProjectWebsite">
-                                    <Form.Label>Profile Website</Form.Label>
-                                    <Form.Control type="text" placeholder="Website url" value={website} onChange={changeWebsite} required />
-                                </Form.Group>
-                            </Col>
-                            <Col lg="4" md="6" sm="12" className="main-col">
-                                <Form.Group className="mb-4" controlId="formProjectHighlights">
-                                    <Form.Label>Project Highlights</Form.Label>
-                                    <Form.Control type="text" placeholder="Why do you like this project?" value={highlight} onChange={changeHighlight} required />
-                                </Form.Group>
-                            </Col>
-                            <Col lg="4" md="6" sm="12" className="main-col">
-                                <Form.Group controlId="formPrice">
-                                    <Form.Label>Price</Form.Label>
-                                    <Form.Control type="text" placeholder="How much?" value={price} onChange={changePrice} required />
-                                </Form.Group>
-                            </Col>
-                            <Col lg="4" md="6" sm="12" className="main-col">
-                                <Form.Group controlId="formProjectDropDate">
-                                    <Form.Label>Project Drop Date</Form.Label>
-                                    <Form.Control type="text" placeholder="When mint?" value={dropDate} onChange={changeDropDate} required/>
-                                </Form.Group>
-                            </Col>
-                            <Col lg="4" md="6" sm="12" className="main-col">
-                                <Form.Group controlId="formProjectDiscord">
-                                    <Form.Label>Project Discord</Form.Label>
-                                    <Form.Control type="text" placeholder="Discord url" value={discord} onChange={changeDiscord} required/>
-                                </Form.Group>
-                            </Col>
-                            <Col lg="4" md="6" sm="12" className="main-col">
-                                <Form.Group controlId="formProjectTwitter">
-                                    <Form.Label>Project Twitter</Form.Label>
-                                    <Form.Control type="text" placeholder="Twitter url" value={twitter} onChange={changeTwitter} required/>
-                                </Form.Group>
-                            </Col>
-                            <Col lg="4" md="6" sm="12" className="main-col">
-                                <Form.Group controlId="formProjectOpensea">
-                                    <Form.Label>Project OpenSea</Form.Label>
-                                    <Form.Control type="text" placeholder="Opensea url" value={opensea} onChange={changeOpensea} required/>
-                                </Form.Group>
-                            </Col>
-                            <Col lg="12" md="12" sm="12" className="main-col">
-                                <Form.Group controlId="formProjectDescription">
-                                    <Form.Label>Project Discription</Form.Label>
-                                    <Form.Control as="textarea" className="footer-element" placeholder="Detailed description of the project" value={description} onChange={changeDescription} />
-                                </Form.Group>
-                            </Col>
-                            <Col lg="3" md="6" sm="12" className="main-col">
-                                <Form.Group className="mb-4">
-                                    <Form.Label>Art Upload</Form.Label>
-                                    <div className="footer-element file-panel">
-                                        <input id="input-file" type="file" name="file" className="d-none" onChange={changeFile} />
-                                        <Button variant="light" id="file-upload-button" onClick={openFile}>{btnTitle}</Button>
-                                    </div>
-                                </Form.Group>
-                            </Col>
-                            <Col lg="9" md="6" className="main-col"></Col>
-                            <Col lg="12" className="main-col padding-bottom-20">
-                                <Link to="/" className="back">&lt;&lt;-back to projects</Link>
-                                <Button variant="secondary" type="submit" className="pull-right">
-                                    <Spinner
-                                    as="span"
-                                    animation={isLoading}
-                                    size="sm"
-                                    role="status"
-                                    aria-hidden="false"
-                                    />
-                                    &nbsp; Submit</Button>
-                            </Col>
-                        </Row>
-                    </Form>
+                    <BlockUi tag="div" blocking={blocking} message={message}>
+                        <Form onSubmit={handleSubmit} encType="multipart/form-data">
+                            <Row>
+                                <Col lg="4" md="6" sm="12" className="main-col">
+                                    <Form.Group controlId="formProjectName">
+                                        <Form.Label>Project Name</Form.Label>
+                                        <Form.Control type="text" placeholder="Name of Project" value={projectName} onChange={changeProjectName} required />
+                                    </Form.Group>
+                                </Col>
+                                <Col lg="4" md="6" sm="12" className="main-col">
+                                    <Form.Group controlId="formSupply">
+                                        <Form.Label>Supply</Form.Label>
+                                        <Form.Control type="text" placeholder="How many?" value={supply} onChange={changeSupply} required />
+                                    </Form.Group>
+                                </Col>
+                                <Col lg="4" md="6" sm="12" className="main-col">
+                                    <Form.Group controlId="formProjectWebsite">
+                                        <Form.Label>Profile Website</Form.Label>
+                                        <Form.Control type="text" placeholder="Website url" value={website} onChange={changeWebsite} required />
+                                    </Form.Group>
+                                </Col>
+                                <Col lg="4" md="6" sm="12" className="main-col">
+                                    <Form.Group className="mb-4" controlId="formProjectHighlights">
+                                        <Form.Label>Project Highlights</Form.Label>
+                                        <Form.Control type="text" placeholder="Why do you like this project?" value={highlight} onChange={changeHighlight} required />
+                                    </Form.Group>
+                                </Col>
+                                <Col lg="4" md="6" sm="12" className="main-col">
+                                    <Form.Group controlId="formPrice">
+                                        <Form.Label>Price</Form.Label>
+                                        <Form.Control type="text" placeholder="How much?" value={price} onChange={changePrice} required />
+                                    </Form.Group>
+                                </Col>
+                                <Col lg="4" md="6" sm="12" className="main-col">
+                                    <Form.Group controlId="formProjectDropDate">
+                                        <Form.Label>Project Drop Date</Form.Label>
+                                        <Form.Control type="text" placeholder="When mint?" value={dropDate} onChange={changeDropDate} required/>
+                                    </Form.Group>
+                                </Col>
+                                <Col lg="4" md="6" sm="12" className="main-col">
+                                    <Form.Group controlId="formProjectDiscord">
+                                        <Form.Label>Project Discord</Form.Label>
+                                        <Form.Control type="text" placeholder="Discord url" value={discord} onChange={changeDiscord} required/>
+                                    </Form.Group>
+                                </Col>
+                                <Col lg="4" md="6" sm="12" className="main-col">
+                                    <Form.Group controlId="formProjectTwitter">
+                                        <Form.Label>Project Twitter</Form.Label>
+                                        <Form.Control type="text" placeholder="Twitter url" value={twitter} onChange={changeTwitter} required/>
+                                    </Form.Group>
+                                </Col>
+                                <Col lg="4" md="6" sm="12" className="main-col">
+                                    <Form.Group controlId="formProjectOpensea">
+                                        <Form.Label>Project OpenSea</Form.Label>
+                                        <Form.Control type="text" placeholder="Opensea url" value={opensea} onChange={changeOpensea} required/>
+                                    </Form.Group>
+                                </Col>
+                                <Col lg="12" md="12" sm="12" className="main-col">
+                                    <Form.Group controlId="formProjectDescription">
+                                        <Form.Label>Project Discription</Form.Label>
+                                        <Form.Control as="textarea" className="footer-element" placeholder="Detailed description of the project" value={description} onChange={changeDescription} />
+                                    </Form.Group>
+                                </Col>
+                                <Col lg="3" md="6" sm="12" className="main-col">
+                                    <Form.Group className="mb-4">
+                                        <Form.Label>Art Upload</Form.Label>
+                                        <div className="footer-element file-panel">
+                                            <input id="input-file" type="file" name="file" className="d-none" onChange={changeFile} />
+                                            <Button variant="light" id="file-upload-button" onClick={openFile}>{btnTitle}</Button>
+                                        </div>
+                                    </Form.Group>
+                                </Col>
+                                <Col lg="9" md="6" className="main-col"></Col>
+                                <Col lg="12" className="main-col padding-bottom-20">
+                                    <Link to="/" className="back">&lt;&lt;-back to projects</Link>
+                                    <Button variant="secondary" type="submit" className="pull-right">
+                                        <Spinner
+                                        as="span"
+                                        animation={isLoading}
+                                        size="sm"
+                                        role="status"
+                                        aria-hidden="false"
+                                        />
+                                        &nbsp; Submit</Button>
+                                </Col>
+                            </Row>
+                        </Form>
+                    </BlockUi>
                 </Row>
             </Container>
         </div>
